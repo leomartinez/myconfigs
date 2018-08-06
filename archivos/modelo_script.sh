eaddbda8bf637021f551f24a1580f7dd8194799a
Leonardo Martinez <leonardo.m.martinez@gmail.com>
2018-08-06 09:17:59 -0300

    Agregada plantilla para scripts de bash

---
+++ archivos/modelo_script.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+## ARCHIVO: /<path>/<script>.sh
+##
+## DESCRIPCIÓN:
+##              
+## USO: <script>.sh <argumentos>
+##
+## AUTOR: Leonardo Martinez <leonardo.m.martinez@gmail.com>
+##
+## FECHA: AAAAMMDD
+##
+## VERSIÓN: 0.1
+##
+## REVISIONES:
+##
+##
+## 0.1 AAAAMMDD Leonardo Martinez <leonardo.m.martinez@gmail.com>
+##                  Primera versión del script.
+##
+
+#############################
+## DEFINICIÓN DE VARIABLES ##
+#############################
+
+# Directorios
+
+
+# Archivos
+
+
+# Comandos
+
+
+# Variables locales
+
+
+# Variables de representación en pantalla
+
+linea="==============================================================================="
+reset_terminal="tput sgr0"
+negrita_ini="\033[1m"
+negrita_fin="\033[0m"
+negro="\E[30m"
+negro_bg="\E[40m"
+rojo="\E[31m"
+rojo_bg="\E[41m"
+verde="\E[32m"
+verde_bg="\E[42m"
+amarillo="\E[33m"
+amarillo_bg="\E[43m"
+azul="\E[34m"
+azul_bg="\E[44m"
+magenta="\E[35m"
+magenta_bg="\E[45m"
+cyan="\E[36m"
+cyan_bg="\E[46m"
+blanco="\E[37m"
+blanco_bg="\E[47m"
+
+
+#############################
+## DEFINICIÓN DE FUNCIONES ##
+#############################
+
+
+#############################
+##    SECCIÓN PRINCIPAL    ##
+#############################
+
+
+#############################
+##    FINAL DEL SCRIPT     ##
+#############################
